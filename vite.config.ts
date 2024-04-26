c56bebde464e95e207abab390f8cfb38e0d28774
oda404 <xdxalexandru404@gmail.com>
2024-04-26 16:10:15 +0300

    vite: Set hmr to work on dev.vspot.ro:5174.
    Read more about these changes and the setup needed to make them work
    on the vspot_stakck repo.

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,11 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		hmr: {
+			host: "dev.vspot.ro",
+			port: 5174
+		}
+	}
 });
