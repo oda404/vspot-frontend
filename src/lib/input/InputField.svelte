a3f7353b0c932408e9021e51d8617e3c64e8ae1c
oda404 <xdxalexandru404@gmail.com>
2024-04-26 16:00:00 +0300

    InputField: Add type attribute.

--- src/lib/input/InputField.svelte
+++ src/lib/input/InputField.svelte
@@ -4,6 +4,7 @@
     export let id: string;
     export let label: string;
     export let data: InputFieldContext;
+    export let type: string | undefined = undefined;
 
     let focused = false;
     $: move_label = focused || data.value.length > 0;
@@ -11,7 +12,9 @@
 
 <div class="flex flex-col space-y-1 relative w-full">
     <label
-        class="absolute {move_label ? 'top-[-8px]' : 'top-[12px]'} {move_label
+        class="absolute transition-all {move_label
+            ? 'top-[-8px]'
+            : 'top-[12px]'} {move_label
             ? 'left-[8px]'
             : 'left-[14px]'} text-vspot-text-hovered bg-vspot-primary-bg px-1"
         for={id}>{label}</label
@@ -25,6 +28,7 @@
         on:blur={() => (focused = false)}
         bind:value={data.value}
         spellcheck={false}
+        {...{ type }}
         class="bg-vspot-primary-bg p-2 px-4 w-full rounded-md border {data.error
             ? 'border-vspot-text-error'
             : 'border-vspot-secondary-bg'} focus:border-vspot-green focus:outline-none"
