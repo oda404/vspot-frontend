cd5247734fd13bfda4b40b53a7f1555a884939b9
oda404 <xdxalexandru404@gmail.com>
2024-05-05 23:04:02 +0300

    Age confirmation: Add underage option.

--- src/lib/ageconfirmation/AgeConfirmationOverlay.svelte
+++ src/lib/ageconfirmation/AgeConfirmationOverlay.svelte
@@ -24,6 +24,7 @@
     <div
         class="fixed flex z-[100] top-0 left-0 w-full h-full bg-[rgba(0,0,0,0.5)] hideScroll justify-center items-center"
     >
+        <div class="hidden">te-a dus capul sa ajungi aici</div>
         <div
             class="bg-vspot-primary-bg p-4 rounded-lg overflow-hidden max-w-[300px] relative"
         >
@@ -32,18 +33,25 @@
                 alt="VSpot logo"
                 class="w-[80px] absolute z-0 right-[-6%] top-[-6%] rotate-[15deg] opacity-15"
             />
-            <div class="text-lg">
+            <span class="text-lg block">
                 {$l("age.confirmation", { n: ageconfirmation_minimum_age })}
-            </div>
-            <div class="text-vspot-text-hovered">
+            </span>
+            <span class="text-vspot-text-hovered">
                 {$l("age.description", { n: ageconfirmation_minimum_age })}
+            </span>
+            <div class="flex space-x-4">
+                <a
+                    class="w-full bg-vspot-text-error p-2 rounded-full text-vspot-text-pimary mt-4 text-center"
+                    href="https://ro.wikipedia.org/wiki/Penitenciarul_Slobozia"
+                    >{$l("action.no")}</a
+                >
+                <button
+                    class="w-full bg-vspot-green p-2 rounded-full text-vspot-primary-bg mt-4"
+                    on:click={() =>
+                        ageconfirmation_store_set(ageconfirmation_minimum_age)}
+                    >{$l("action.yes")}</button
+                >
             </div>
-            <button
-                class="w-full bg-vspot-green p-2 rounded-full text-vspot-primary-bg mt-4"
-                on:click={() =>
-                    ageconfirmation_store_set(ageconfirmation_minimum_age)}
-                >{$l("action.yes")}</button
-            >
         </div>
     </div>
 {/if}
