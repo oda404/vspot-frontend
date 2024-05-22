27cda1d4d869462086d98388f2a65520b9120c51
oda404 <xdxalexandru404@gmail.com>
2024-05-23 00:33:17 +0300

    NavigatingOverlay: Place spinner in the bottom left of the page.

--- src/lib/layout/NavigatingOverlay.svelte
+++ src/lib/layout/NavigatingOverlay.svelte
@@ -2,10 +2,8 @@
     import Spinner from "$lib/Spinner.svelte";
 </script>
 
-<div
-    class="fixed flex z-[100] top-0 left-0 w-full h-full bg-[rgba(0,0,0,0.5)] hideScroll justify-center items-center"
->
-    <div class="w-[100px] h-[100px]">
+<div class="fixed flex z-[100] top-0 left-0 w-full h-full hideScroll">
+    <div class="mt-auto mb-[40px] ml-[40px]">
         <Spinner scale={2} fg="#eeeeee" />
     </div>
 </div>
