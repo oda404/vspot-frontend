5ed132d2d16646620460611eb00d849a43c3b3ae
oda404 <xdxalexandru404@gmail.com>
2024-07-06 15:49:43 +0300

    product page: Improve style and UX.

---
+++ src/lib/shipping/estimation.ts
@@ -0,0 +1,45 @@
+
+
+export function shipping_estimation_get_date_from_now() {
+    const weekday_names = [
+        "Luni",
+        "Marti",
+        "Miercuri",
+        "Joi",
+        "Vineri",
+    ];
+
+    const month_names = [
+        "Ianuarie",
+        "Februarie",
+        "Martie",
+        "Aprilie",
+        "Mai",
+        "Iunie",
+        "Iulie",
+        "August",
+        "Septembrie",
+        "Octombrie",
+        "Noiembrie",
+        "Decembrie",
+    ];
+
+    const now = new Date();
+    let days_to_deliver = 1;
+    if (now.getDay() === 5) {
+        days_to_deliver += now.getHours() > 13 ? 3 : 2;
+    }
+    else if (now.getDay() === 6) {
+        days_to_deliver += 2;
+    }
+    else if (now.getDay() === 7) {
+        days_to_deliver += 1;
+    }
+    else if (now.getHours() > 13) {
+        ++days_to_deliver;
+    }
+
+    now.setDate(now.getDate() + days_to_deliver);
+    return `Poate ajunge la tine: ${weekday_names[now.getDay() - 1]}, ${now.getDate()} ${month_names[now.getMonth()]}`;
+}
+
--- src/routes/(client)/product/[id]/+page.svelte
+++ src/routes/(client)/product/[id]/+page.svelte
@@ -1,13 +1,22 @@
 <script lang="ts">
-    import { cart_add_item } from "$lib/cart/cart";
+    import { cart_add_item, cart_store } from "$lib/cart/cart";
     import { l } from "$lib/langs";
+    import { price_discounted_val, price_format } from "$lib/price.js";
     import Product from "$lib/products/Product.svelte";
     import ProductTrailBar from "$lib/products/ProductTrailBar.svelte";
     import { product_price_get_percdiff } from "$lib/products/price.js";
+    import { shipping_estimation_get_date_from_now } from "$lib/shipping/estimation";
     import { pagetitle_make } from "$lib/title";
-    import { faDollar } from "@fortawesome/free-solid-svg-icons";
+    import {
+        faChevronLeft,
+        faChevronRight,
+        faDollar,
+        faTruck,
+    } from "@fortawesome/free-solid-svg-icons";
+    import { onDestroy } from "svelte";
     import Fa from "svelte-fa";
     import SvelteMarkdown from "svelte-markdown";
+    import { get } from "svelte/store";
 
     export let data;
 
@@ -18,6 +27,30 @@
 
     const price_full = data.product.price;
     const price_actual = price_full - data.product.discount;
+
+    let qty = 1;
+    $: {
+        const cart_product = get(cart_store).items.find(
+            (prod) => prod.internal_id === data.product.internal_id,
+        );
+
+        const max_qty = data.product.stock - (cart_product?.qty || 0);
+
+        if (qty < 0) qty = 1;
+        else if (qty > max_qty) qty = max_qty;
+    }
+
+    /* Make qty 1 if qty is 0 and the user remove stuff from their cart */
+    onDestroy(
+        cart_store.subscribe(($cart_store) => {
+            const cart_qty =
+                $cart_store.items.find(
+                    (prod) => prod.internal_id === data.product.internal_id,
+                )?.qty || 0;
+
+            if (cart_qty < data.product.stock && qty === 0) qty = 1;
+        }),
+    );
 
     type Tab = "description" | "specs";
     let selected_tab: Tab = "description";
@@ -39,14 +72,14 @@
                 src={data.product.image_url}
                 alt="{data.product.name} image"
             />
-            <div class="space-y-1 mt-4">
+            <div class="space-y-1 w-full mt-4 lg:mt-0">
                 <div class="space-y-2">
                     {#if data.product.discount > 0}
                         <div class="flex items-center space-x-2">
                             <span
-                                class="line-through text-2xl text-vspot-text-hovered"
+                                class="line-through text-xl text-vspot-text-hovered"
                             >
-                                {data.product.price}.00
+                                {price_format(data.product.price)} RON
                             </span>
                             <span
                                 class="px-2 p-[0.1rem] rounded-tl-lg rounded-br-lg bg-vspot-secondary-bg text-lg"
@@ -59,13 +92,23 @@
                         </div>
                     {/if}
                     <span
-                        class="text-4xl block font-semibold text-vspot-text-hovered"
+                        class="text-3xl block font-semibold text-vspot-text-hovered"
                     >
-                        {data.product.price - data.product.discount}
-                        <span class="text-2xl font-semibold -ml-3">.00</span>
-                        {data.product.currency}
+                        {price_format(
+                            price_discounted_val(
+                                data.product.price,
+                                data.product.discount,
+                            ),
+                        )} RON
                     </span>
                 </div>
+                {#if price_actual !== price_full}
+                    <div class="flex items-center space-x-2">
+                        <Fa icon={faDollar} />
+                        <span>{$l("price.only_online")}</span>
+                    </div>
+                {/if}
+                <div class="h-[1px] w-full bg-vspot-secondary-bg !my-4" />
                 {#if data.product.stock}
                     <div class="flex items-center space-x-2">
                         <div
@@ -80,25 +123,69 @@
                         </span>
                     </div>
                 {/if}
-                {#if price_actual !== price_full}
-                    <div class="flex items-center space-x-2">
-                        <Fa icon={faDollar} />
-                        <span>Acest pret este valabil exclusiv online</span>
+                <div class="flex items-center space-x-2 !mt-4">
+                    {#if data.product.stock}
+                        <div class="flex items-center">
+                            <button
+                                class="h-[calc(50%)] p-1 px-4 rounded-none"
+                                disabled={qty <= 1}
+                                on:click={() => {
+                                    --qty;
+                                }}
+                            >
+                                <Fa
+                                    color={qty <= 1 ? "#cccccc" : ""}
+                                    icon={faChevronLeft}
+                                />
+                            </button>
+                            <input
+                                class="border text-center border-vspot-secondary-bg text-lg size-12 bg-vspot-primary-bg focus:outline-none"
+                                on:blur={() => {
+                                    if (qty <= 0) qty = 1;
+                                }}
+                                bind:value={qty}
+                            />
+                            <button
+                                class="h-[calc(50%)] p-1 px-4 rounded-none"
+                                disabled={qty >= data.product.stock}
+                                on:click={() => {
+                                    ++qty;
+                                }}
+                            >
+                                <Fa
+                                    color={qty >= data.product.stock
+                                        ? "#cccccc"
+                                        : ""}
+                                    icon={faChevronRight}
+                                />
+                            </button>
+                        </div>
+                    {/if}
+                    <button
+                        disabled={data.product.stock === 0}
+                        class="text-lg px-16 w-full p-2 bg-vspot-green text-vspot-primary-bg disabled:bg-vspot-secondary-bg disabled:text-vspot-text-hovered"
+                        on:click={() => {
+                            cart_add_item(
+                                data.product.internal_id,
+                                undefined,
+                                true,
+                                Number(qty),
+                            );
+                        }}
+                    >
+                        {#if data.product.stock === 0}
+                            {$l("description.outofstock_simple")}
+                        {:else}
+                            {$l("action.addtocart")}
+                        {/if}
+                    </button>
+                </div>
+                {#if data.product.stock}
+                    <div class="flex space-x-2 items-center !mt-8">
+                        <Fa icon={faTruck} />
+                        <span>{shipping_estimation_get_date_from_now()}</span>
                     </div>
                 {/if}
-                <button
-                    disabled={data.product.stock === 0}
-                    class="px-16 w-fit p-2 !mt-4 bg-vspot-green text-vspot-primary-bg disabled:bg-vspot-secondary-bg disabled:text-vspot-text-hovered"
-                    on:click={() => {
-                        cart_add_item(data.product.internal_id);
-                    }}
-                >
-                    {#if data.product.stock === 0}
-                        {$l("description.outofstock_simple")}
-                    {:else}
-                        {$l("action.addtocart")}
-                    {/if}
-                </button>
             </div>
         </div>
     </div>
@@ -132,7 +219,7 @@
     {/if}
     {#if recommended}
         <div class="space-y-4 border-t border-vspot-secondary-bg py-8">
-            <span class="text-2xl">Recomandate pentru tine</span>
+            <span class="text-2xl">{$l("product.recommended_for_you")}</span>
             <div
                 class="flex space-x-4
                 [&>*:nth-child(6)]:hidden
