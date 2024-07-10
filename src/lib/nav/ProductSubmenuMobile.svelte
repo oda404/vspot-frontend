6f37a4c8d7d2a320917f7c00aedd06ea2f4969b3
oda404 <xdxalexandru404@gmail.com>
2024-07-10 14:29:35 +0300

    ProductSubmenuMobile: Deleted space-x-4 by accident.

--- src/lib/nav/ProductSubmenuMobile.svelte
+++ src/lib/nav/ProductSubmenuMobile.svelte
@@ -23,7 +23,9 @@
     tabindex="0"
     class="z-20 flex flex-col fixed lg:hidden w-[60vw] h-[calc(100vh-20px)] left-[-20px] top-[10px] p-4 rounded-lg bg-vspot-primary-bg space-y-4"
 >
-    <div class="flex items-center border-b border-vspot-secondary-bg pb-2">
+    <div
+        class="flex space-x-4 items-center border-b border-vspot-secondary-bg pb-2"
+    >
         {#if show_back}
             <button
                 on:click={() => {
