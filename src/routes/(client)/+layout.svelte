61e03c8a61d20ae61f73f19cd7e9c42b511f2078
Alexandru Olaru <xdxalexandru404@gmail.com>
2024-09-07 22:59:32 +0300

    layout: Add preconnect to backend.

--- src/routes/(client)/+layout.svelte
+++ src/routes/(client)/+layout.svelte
@@ -1,9 +1,19 @@
 <script lang="ts">
+    import {
+        PUBLIC_ENV,
+        PUBLIC_VSPOT_BACKEND_GLOBAL_URL,
+    } from "$env/static/public";
     import LayoutMain from "$lib/layout/LayoutMain.svelte";
     import "../../app.css";
 
     export let data;
 </script>
+
+<svelte:head>
+    {#if PUBLIC_ENV === "prod"}
+        <link rel="preconnect" href={PUBLIC_VSPOT_BACKEND_GLOBAL_URL} />
+    {/if}
+</svelte:head>
 
 <LayoutMain user={data.user}>
     <slot />
